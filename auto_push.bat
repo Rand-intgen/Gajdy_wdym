88e103ac7acb48e0d7f110bceab11063fc9d07e4
Štěpán Gajdacz <stepan.gajdacz@TRIA.local>
2026-03-10 13:06:51 +0100

    Organizace vylepšení

--- auto_push.bat
+++ auto_push.bat
@@ -5,6 +5,9 @@ git add .
 echo Vytvarim commit s prednastavenou zpravou...
 git commit -m "Automaticky ulozeno a odeslano (auto_push)"
 
+echo Stahuji aktualizace ze vzdaleneho repozitare (git pull)...
+git pull origin main --rebase
+
 echo Odesilam na GitHub (git push)...
 git push origin main
 
